a24d483755eac3b8fa55f5af1bea2a3165b554f3
kwanho <jinkwanho14@naver.com>
2024-05-21 15:36:12 +0900

    @EnableScheduling 제거

--- src/main/java/com/tree/gdhealth/GdhealthApplication.java
+++ src/main/java/com/tree/gdhealth/GdhealthApplication.java
@@ -3,9 +3,7 @@ package com.tree.gdhealth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @ServletComponentScan
 @SpringBootApplication
 public class GdhealthApplication {
